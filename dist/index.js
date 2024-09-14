8dfb6ea42c2c3484fa034a475852d45d7f8e3b3c
Shuashuaa <jovijoshuatania027@gmail.com>
2024-09-14 20:35:02 +0800

    091424-v0.5.4

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -8,83 +9,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-nocheck
-// @ts-ignore
-var fs_1 = require("fs");
-// @ts-ignore
-var path_1 = require("path");
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const yargs_1 = __importDefault(require("yargs"));
+const chalk_1 = __importDefault(require("chalk"));
+const readline_1 = require("readline");
+const readline = (0, readline_1.createInterface)({
+    input: process.stdin,
+    output: process.stdout
+});
 function copyProjectStructure(sourcePath, destinationPath) {
-    return __awaiter(this, void 0, void 0, function () {
-        var entries, _i, entries_1, entry, sourceFile, destinationFile;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, fs_1.default.promises.readdir(sourcePath, { withFileTypes: true })];
-                case 1:
-                    entries = _a.sent();
-                    _i = 0, entries_1 = entries;
-                    _a.label = 2;
-                case 2:
-                    if (!(_i < entries_1.length)) return [3 /*break*/, 8];
-                    entry = entries_1[_i];
-                    sourceFile = path_1.default.join(sourcePath, entry.name);
-                    destinationFile = path_1.default.join(destinationPath, entry.name);
-                    if (!entry.isDirectory()) return [3 /*break*/, 5];
-                    return [4 /*yield*/, fs_1.default.promises.mkdir(destinationFile)];
-                case 3:
-                    _a.sent();
-                    return [4 /*yield*/, copyProjectStructure(sourceFile, destinationFile)];
-                case 4:
-                    _a.sent();
-                    return [3 /*break*/, 7];
-                case 5: return [4 /*yield*/, fs_1.default.promises.copyFile(sourceFile, destinationFile)];
-                case 6:
-                    _a.sent();
-                    _a.label = 7;
-                case 7:
-                    _i++;
-                    return [3 /*break*/, 2];
-                case 8: return [2 /*return*/];
+    return __awaiter(this, void 0, void 0, function* () {
+        const entries = yield fs_1.default.promises.readdir(sourcePath, { withFileTypes: true });
+        for (const entry of entries) {
+            const sourceFile = path_1.default.join(sourcePath, entry.name);
+            const destinationFile = path_1.default.join(destinationPath, entry.name);
+            if (entry.isDirectory()) {
+                yield fs_1.default.promises.mkdir(destinationFile);
+                yield copyProjectStructure(sourceFile, destinationFile);
             }
-        });
+            else {
+                yield fs_1.default.promises.copyFile(sourceFile, destinationFile);
+            }
+        }
     });
 }
-var projectName = 'sample';
-var sourcePath = 'v-kit';
-var destinationPath = path_1.default.join(process.cwd(), projectName);
-try {
-    // @ts-ignore
-    await fs_1.default.promises.mkdir(destinationPath);
-    // @ts-ignore
-    await copyProjectStructure(sourcePath, destinationPath);
-}
-catch (error) {
-    console.error("Error creating project: ".concat(error.message));
+createProject();
+function createProject() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Check if a project name is provided through arguments
+        const argv = yargs_1.default.argv;
+        let projectName = process.argv[2];
+        if (!projectName) {
+            // Prompt the user for a project name
+            projectName = yield new Promise((resolve) => {
+                readline.question('Enter a project name (using default "v-kit-app" if left blank): ', (answer) => {
+                    resolve(answer || 'v-kit-app'); // Assign default if answer is empty
+                });
+            });
+        }
+        // const sourcePath = 'tmp/v-kit';
+        const sourcePath = path_1.default.join(__dirname, '../template/v-kit');
+        const destinationPath = path_1.default.join(process.cwd(), projectName);
+        try {
+            yield fs_1.default.promises.mkdir(destinationPath);
+            yield copyProjectStructure(sourcePath, destinationPath);
+            console.log(chalk_1.default.green('✨ Project created successfully! ✨\n'));
+            console.log(chalk_1.default.green('A Boilerplates created with coffee & passion by', chalk_1.default.green.bold.underline('Shuashuaa'), '\nhttps://github.com/Shuashuaa/v-kit\n'));
+            console.log(chalk_1.default.bgBlack('> Install Dependencies: '), chalk_1.default.bold.underline('npm install'), 'or', chalk_1.default.bold.underline('yarn install'));
+            console.log(chalk_1.default.bgBlack('> Run Project: '), chalk_1.default.bold.underline('npm run dev'), 'or', chalk_1.default.bold.underline('yarn dev'), 'and', chalk_1.default.bold.underline('php artisan serve\n'));
+            // console.log(chalk.bgBlack('Woohoo! Bom Bom Bom Bom!, Enjoy! 🎉🎉'));
+            readline.close();
+        }
+        catch (error) {
+            console.error(chalk_1.default.red.inverse(`Error creating project: ${error.message}`));
+            readline.close();
+        }
+    });
 }
