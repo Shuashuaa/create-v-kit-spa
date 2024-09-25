e69f720bb823d78dea3169f03fe5fe27e1a2b977
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-09-25 21:25:27 +0800

    092524-v0.6.3

--- dist/messages.js
+++ dist/messages.js
@@ -13,7 +13,7 @@ function successMessage() {
     console.log(chalk_1.default.cyan.bold("        ˜                  |"));
     console.log(chalk_1.default.red.bold("    ()    .-.,='``'=.    - o -"));
     console.log(chalk_1.default.yellow.bold("          '=/_       〵    |"));
-    console.log(chalk_1.default.cyanBright.bold("       *   |  '=._     )"));
+    console.log(chalk_1.default.cyanBright.bold("       *   |  '=._    |"));
     console.log(chalk_1.default.yellow.bold("           \\      `=../`,        '"));
     console.log(chalk_1.default.redBright.bold("         .   '=.__.=' `='      *"));
     console.log(chalk_1.default.yellow.bold("˜                         +"));
@@ -37,7 +37,7 @@ function successMessage() {
     //             .   '=.__.=' `='      *
     //    +                         +
     //         o      *        '       .
-    console.log(chalk_1.default.green('A Boilerplates is created with coffee & passion by', chalk_1.default.green.bold.underline('Shuashuaa'), chalk_1.default.whiteBright('\nFor more info: >>'), chalk_1.default.green.underline('https://github.com/Shuashuaa/v-kit\n')));
+    console.log(chalk_1.default.green('A Boilerplates is created with coffee & passion by', chalk_1.default.green.bold.underline('Shuashuaa'), chalk_1.default.whiteBright('\nFor more info: >>'), chalk_1.default.green.underline('https://github.com/Shuashuaa/v-kit-spa\n')));
     console.log('Well done 🎉🎉,\n');
     console.log(chalk_1.default.bgBlack('Install Dependencies:'), chalk_1.default.cyanBright('npm install'), 'or', chalk_1.default.cyanBright('yarn install'), 'and', chalk_1.default.cyanBright('composer install') + '.');
     console.log(chalk_1.default.bgBlack('Run Project:'), chalk_1.default.cyanBright('php artisan serve'));
