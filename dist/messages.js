4d47e5346d00a82490a6c4072d965b29339429af
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-09-25 21:24:57 +0800

    092524-v0.6.3

--- dist/messages.js
+++ dist/messages.js
@@ -7,21 +7,21 @@ exports.successMessage = successMessage;
 exports.errorMessage = errorMessage;
 const chalk_1 = __importDefault(require("chalk"));
 function successMessage() {
-    console.log(chalk_1.default.green('✔  Project created successfully! \n'));
+    console.log(chalk_1.default.green('\n✔  Project created successfully! \n'));
     // console.log(chalk.yellow('     /\_/\ \n    ( o.o )\n     >^<\n'));
-    console.log(chalk_1.default.yellow.bold("              *       +"));
-    console.log(chalk_1.default.cyan.bold("        '                  |"));
-    console.log(chalk_1.default.magenta.bold("    ()    .-.,='``'=.    - o -"));
-    console.log(chalk_1.default.yellow.bold("          '=/_       \\     |"));
-    console.log(chalk_1.default.cyanBright.bold("       *   |  '=._    |"));
+    console.log(chalk_1.default.yellow.bold("              *       ˜"));
+    console.log(chalk_1.default.cyan.bold("        ˜                  |"));
+    console.log(chalk_1.default.red.bold("    ()    .-.,='``'=.    - o -"));
+    console.log(chalk_1.default.yellow.bold("          '=/_       〵    |"));
+    console.log(chalk_1.default.cyanBright.bold("       *   |  '=._     )"));
     console.log(chalk_1.default.yellow.bold("           \\      `=../`,        '"));
-    console.log(chalk_1.default.magentaBright.bold("         .   '=.__.=' `='      *"));
-    console.log(chalk_1.default.yellow.bold("+                         +"));
+    console.log(chalk_1.default.redBright.bold("         .   '=.__.=' `='      *"));
+    console.log(chalk_1.default.yellow.bold("˜                         +"));
     console.log(chalk_1.default.cyanBright.bold("     O      *        '       .\n"));
     // console.log(chalk.yellow("              *       +"));
     // console.log(chalk.yellow("        '                  |"));
     // console.log(chalk.yellow("    ()    .-.,='``'=.    - o -"));
-    // console.log(chalk.yellow("          '=/_       \\     |"));
+    // console.log(chalk.yellow("          '=/_       〵     |"));
     // console.log(chalk.yellow("       *   |  '=._    |"));
     // console.log(chalk.yellow("           \\      `=../`,        '     ╱|\\\\"));
     // console.log(chalk.yellow("         .   '=.__.=' `='      *      (˚ˎ。7  "));
@@ -38,12 +38,12 @@ function successMessage() {
     //    +                         +
     //         o      *        '       .
     console.log(chalk_1.default.green('A Boilerplates is created with coffee & passion by', chalk_1.default.green.bold.underline('Shuashuaa'), chalk_1.default.whiteBright('\nFor more info: >>'), chalk_1.default.green.underline('https://github.com/Shuashuaa/v-kit\n')));
-    console.log('Well done,\n');
+    console.log('Well done 🎉🎉,\n');
     console.log(chalk_1.default.bgBlack('Install Dependencies:'), chalk_1.default.cyanBright('npm install'), 'or', chalk_1.default.cyanBright('yarn install'), 'and', chalk_1.default.cyanBright('composer install') + '.');
-    console.log(chalk_1.default.bgBlack('Run Project:'), chalk_1.default.cyanBright('npm run dev'), 'or', chalk_1.default.cyanBright('yarn dev'));
-    console.log(chalk_1.default.bgBlack('And in a separated command line, Run', chalk_1.default.cyanBright('php artisan serve') + '.\n'));
-    // console.log(chalk.bgBlack('Woohoo! Bom Bom Bom Bom!, Enjoy! 🎉🎉'));
+    console.log(chalk_1.default.bgBlack('Run Project:'), chalk_1.default.cyanBright('php artisan serve'));
+    console.log(chalk_1.default.bgBlack('And in a separated command line, Run', chalk_1.default.cyanBright('yarn watch') + '.\n'));
+    console.log('Visit:', chalk_1.default.underline('https://127.0.0.1:8000') + '.');
 }
 function errorMessage(error) {
-    console.error(chalk_1.default.red.inverse(`Error creating project: ${error.message}`));
+    console.error(chalk_1.default.red.inverse(`\n✖ Error creating project. ${error.message}`));
 }
