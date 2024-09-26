64a210975d34c64eebf6ee298e6c8c1574cdaa8f
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-09-26 20:08:54 +0800

    092624-v0.7.1

--- dist/prompts.d.ts
+++ dist/prompts.d.ts
@@ -1,3 +1,4 @@
 export declare function getProjectName(): Promise<string>;
+export declare function getBoilerplateChoice(): Promise<number>;
 export declare function closeReadline(): void;
 //# sourceMappingURL=prompts.d.ts.map
