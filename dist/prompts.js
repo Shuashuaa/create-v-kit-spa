0b19b0ae5bc63bd0a805da2063ac585a0c73f461
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-09-26 20:09:24 +0800

    092624-v0.7.1

--- dist/prompts.js
+++ dist/prompts.js
@@ -8,9 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getProjectName = getProjectName;
+exports.getBoilerplateChoice = getBoilerplateChoice;
 exports.closeReadline = closeReadline;
+const chalk_1 = __importDefault(require("chalk"));
 const readline_1 = require("readline");
 const readline = (0, readline_1.createInterface)({
     input: process.stdin,
@@ -19,8 +24,29 @@ const readline = (0, readline_1.createInterface)({
 function getProjectName() {
     return __awaiter(this, void 0, void 0, function* () {
         return new Promise((resolve) => {
-            readline.question('Enter a project name (using default "v-kit-app" if left blank): ', (answer) => {
-                resolve(answer || 'v-kit-app'); // Assign default if answer is empty
+            // readline.question('? Enter a project name (using default "v-kit-app" if left blank): ', (answer) => {
+            //   resolve(answer || 'v-kit-app');
+            // });
+            let message = `\n${chalk_1.default.bold.cyan('?')} ${chalk_1.default.bold('Enter a project name')} (using default "${chalk_1.default.bold.magentaBright('v-kit-app')}" if left blank): `;
+            readline.question(message, (answer) => {
+                resolve(answer || 'v-kit-app');
+            });
+        });
+    });
+}
+function getBoilerplateChoice() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve) => {
+            let message = `\n${chalk_1.default.bold.cyan('?')} ${chalk_1.default.bold('Select your preferred boilerplate:')}\n`;
+            message += `\n        MonoRepo:              Decoupled:\n`;
+            message += `[1] - ${chalk_1.default.bold.underline('Vue2-Laravel8')}  [6] - ${chalk_1.default.bold.underline.gray('Vue2-Laravel8')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            message += `[2] - ${chalk_1.default.bold.underline('Vue3-Laravel8')}  [7] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel8')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            message += `[3] - ${chalk_1.default.bold.underline('Vue3-Laravel9')}  [8] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel9')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            message += `[4] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel10')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            message += `[5] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel11')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            message += '=> ';
+            readline.question(message, (answer) => {
+                resolve(parseInt(answer));
             });
         });
     });
