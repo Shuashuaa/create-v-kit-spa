bc4c3eda2dd9c24e823e7c3467e5fe814e47bb93
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-09-26 20:03:57 +0800

    092624-v0.7.1

---
+++ templates/158/storage/framework/views/4819e26219ba3fdb0b55b7f698da8ac579e14006.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+    <title>Vue-Laravel SPA App</title>
+</head>
+<body>
+    <div id="app"></div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html><?php /**PATH C:\Users\est074\Desktop\l\resources\views/layouts/app.blade.php ENDPATH**/ ?>
