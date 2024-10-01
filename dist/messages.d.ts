744eaae8fce4f06009286ab9048a0e23e881776b
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-10-01 21:13:58 +0800

    100124-v0.9.4

--- dist/messages.d.ts
+++ dist/messages.d.ts
@@ -1,3 +1,3 @@
-export declare function successMessage(): void;
+export declare function successMessage(boilerplateChoice: string): void;
 export declare function errorMessage(error: Error): void;
 //# sourceMappingURL=messages.d.ts.map
