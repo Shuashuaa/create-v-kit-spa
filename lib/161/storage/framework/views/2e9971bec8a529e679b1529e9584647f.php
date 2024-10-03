c882b712710caf3fcc1035cfb26af1f32c97f407
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-10-03 20:45:42 +0800

    100324-v0.9.9

---
+++ lib/161/storage/framework/views/2e9971bec8a529e679b1529e9584647f.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue 3 - Laravel 10 SPA</title>
+
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+    
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html><?php /**PATH C:\Users\est074\Desktop\v-kit\v-kit\lib\161\resources\views/layouts/app.blade.php ENDPATH**/ ?>
