04e88504559fe69cc75522d1f7d1bb323321ad9e
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-10-05 10:39:36 +0800

    100524-v1.0.0-alpha

--- dist/prompts.js
+++ dist/prompts.js
@@ -38,10 +38,14 @@ function getBoilerplateChoice() {
     return __awaiter(this, void 0, void 0, function* () {
         return new Promise((resolve) => {
             let message = `\n${chalk_1.default.bold.cyan('?')} ${chalk_1.default.bold('Select your preferred boilerplate:')}\n`;
-            message += `\nMonoRepo:                         Decoupled:\n`;
-            message += `[1] - ${chalk_1.default.bold.underline('Vue2-Laravel8')}               [6] - ${chalk_1.default.bold.underline.gray('Vue2-Laravel8')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
-            message += `[2] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel8')} ${chalk_1.default.gray.dim.italic('(on-going..)')}  [7] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel8')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
-            message += `[3] - ${chalk_1.default.bold.underline('Vue3-Laravel9')}               [8] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel9')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            // message += `\nMonoRepo:\n`;                         
+            // Decoupled:\n`;
+            message += `\n[1] - ${chalk_1.default.bold.underline('Vue2-Laravel8')}\n`;
+            // [6] - ${chalk.bold.underline.gray('Vue2-Laravel8')} ${chalk.gray.dim.italic('(on-going..)')};
+            message += `[2] - ${chalk_1.default.bold.underline.gray('Vue3-Laravel8')} ${chalk_1.default.gray.dim.italic('(on-going..)')}\n`;
+            // [7] - ${chalk.bold.underline.gray('Vue3-Laravel8')} ${chalk.gray.dim.italic('(on-going..)')}\n`; 
+            message += `[3] - ${chalk_1.default.bold.underline('Vue3-Laravel9')}\n`;
+            // [8] - ${chalk.bold.underline.gray('Vue3-Laravel9')} ${chalk.gray.dim.italic('(on-going..)')}\n`;
             message += `[4] - ${chalk_1.default.bold.underline('Vue3-Laravel10')}\n`;
             message += `[5] - ${chalk_1.default.bold.underline('Vue3-Laravel11')}\n`;
             message += '=> ';
