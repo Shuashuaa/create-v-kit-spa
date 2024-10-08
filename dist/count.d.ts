d627ea19555cdebe5dcaf4f3b5fd82a730924599
Shuashuaa <Shuashuaa@users.noreply.github.com>
2024-10-08 21:46:25 +0800

    100824-v1.0.1

---
+++ dist/count.d.ts
@@ -0,0 +1,3 @@
+export declare function countItems(dirPath: string): Promise<number>;
+export declare function getCount(sourcePath: string, templatePath: string): void;
+//# sourceMappingURL=count.d.ts.map
